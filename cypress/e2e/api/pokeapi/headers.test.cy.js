64a99e258ecc994b3e0d3fc845439aab9c58afa2
MDimas27 <mdimas.umb@gmail.com>
2023-01-25 11:28:41 +0700

    update tugas section 9 - validate content

--- cypress/e2e/api/pokeapi/headers.test.cy.js
+++ cypress/e2e/api/pokeapi/headers.test.cy.js
@@ -1,16 +1,34 @@
-describe('Validate Header', () => {
+describe('Automation API with PokeAPI', () => {
     it('Successfully validate content-type', () => {
-        cy.fixture("pokeapi")
-            .request({
+        cy.fixture("pokeapi").then((user) => {
+            cy.request({
                 method: "GET",
                 url: 'https://pokeapi.co/api/v2/pokemon/ditto',
             }).as('pokemon')
-            .then((response) => {
-                expect(response.body.abilities[0].ability.name).to.eq('limber')
-                cy.log(JSON.stringify(response.body))
-            })
+                .then((response) => {
+                    expect(response.body.abilities[0].ability.name).to.eq(user.abilities[0].ability.name)
+                    cy.log(JSON.stringify(response.body))
+                })
             cy.get('@pokemon').its('headers').its('content-type')
                 .should('include', 'application/json; charset=utf-8')
-            cy.get('@pokemon').its('status').should('equal', 200)
+            // cy.get('@pokemon').its('status').should('equal', 200)
+        })
     })
+    it('Successfully validate status code', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('ditto')
+        cy.get('@ditto').its('status').should('equal', 200)
+    });
+
+    it('Successfully validate status code with params', () => {
+        cy.request({
+            method: "GET",
+            url: 'https://pokeapi.co/api/v2/pokemon/ditto',
+            }).as('ditto2')
+        cy.get('@ditto2').its('status').should('equal', 200)
+    });
+
+    it('Successfully validate content', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/bulbasaur').as('bulbasaur')
+        cy.get('@bulbasaur').its('body').should('include', {name: 'bulbasaur'})
+    });
 });
